db2323dfa4d7af5c645466229fb0c2d28a2aa4ee
Mohammed Ghazal <drghazalm@gmail.com>
2024-06-04 07:39:56 +0400

    Vanilla App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { TrackComponent } from './features/track/track.component';
-import { DefineComponent } from './features/define/define.component';
+
 export const routes: Routes = [
-    {path:'', component:HomeComponent},
-    {path:'define', component:DefineComponent},
-    {path:'track', component:TrackComponent}
+    {path:'', component:HomeComponent}
 ];
