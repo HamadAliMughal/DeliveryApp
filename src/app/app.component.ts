006afe859de47ec9ac81198f6847e7c7e8491053
bushraaaaaaaaa <81895046+HamadAliMughal@users.noreply.github.com>
2024-06-04 15:51:28 +0500

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,24 @@ import { MHeaderComponent } from './m-framework/m-header/m-header.component';
 import { MCardComponent } from './m-framework/m-card/m-card.component';
 import { MContainerComponent } from './m-framework/m-container/m-container.component';
 import { CommonModule } from '@angular/common';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfkkoEisi7qJkSucYhZ0v2zUSXspmPJF8",
+  authDomain: "deliveryapp-5bedb.firebaseapp.com",
+  projectId: "deliveryapp-5bedb",
+  storageBucket: "deliveryapp-5bedb.appspot.com",
+  messagingSenderId: "754271695647",
+  appId: "1:754271695647:web:7faec24cd2f2937c3b762a",
+  measurementId: "G-44QFFSXZ8L"
+};
 
 @Component({
   selector: 'app-root',
@@ -14,7 +31,9 @@ import { CommonModule } from '@angular/common';
     MCardComponent,
     MHeaderComponent,
     RouterOutlet,
-    CommonModule
+    CommonModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAnalyticsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyCfkkoEisi7qJkSucYhZ0v2zUSXspmPJF8",
+      authDomain: "deliveryapp-5bedb.firebaseapp.com",
+      projectId: "deliveryapp-5bedb",
+      storageBucket: "deliveryapp-5bedb.appspot.com",
+      messagingSenderId: "754271695647",
+      appId: "1:754271695647:web:7faec24cd2f2937c3b762a",
+      measurementId: "G-44QFFSXZ8L"
+    }
+  };
+  
