4ee770a6d77e2cbf595cdedeaa9e6252df3d2747
hamad <81895046+HamadAliMughal@users.noreply.github.com>
2024-06-04 16:01:26 +0500

    Revert "testing"
    This reverts commit 006afe859de47ec9ac81198f6847e7c7e8491053.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,7 @@ import { MHeaderComponent } from './m-framework/m-header/m-header.component';
 import { MCardComponent } from './m-framework/m-card/m-card.component';
 import { MContainerComponent } from './m-framework/m-container/m-container.component';
 import { CommonModule } from '@angular/common';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCfkkoEisi7qJkSucYhZ0v2zUSXspmPJF8",
-  authDomain: "deliveryapp-5bedb.firebaseapp.com",
-  projectId: "deliveryapp-5bedb",
-  storageBucket: "deliveryapp-5bedb.appspot.com",
-  messagingSenderId: "754271695647",
-  appId: "1:754271695647:web:7faec24cd2f2937c3b762a",
-  measurementId: "G-44QFFSXZ8L"
-};
 
 @Component({
   selector: 'app-root',
@@ -31,9 +14,7 @@ const firebaseConfig = {
     MCardComponent,
     MHeaderComponent,
     RouterOutlet,
-    CommonModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireAnalyticsModule
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
